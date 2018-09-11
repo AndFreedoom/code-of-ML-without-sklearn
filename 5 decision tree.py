cdec8e727e7d28d665d7f8d30624b34da7cd1ed4
AndFreedoom <[liuyingxinwy@163.com]>
2018-09-11 18:39:50 +0800

    5

--- 5 decision tree.py
+++ 5 decision tree.py
@@ -1,1 +1,120 @@
-import numpy as np
+
+
+
+'''代码为ID3，旨在理解划分特征选择，切分数据等决策树构建过程，因此没有可视化部分代码'''
+from math import log
+import operator
+
+
+'''计算香农熵'''
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        # labelCounts[currentLabel] = labelCounts.get(currentLabel, 0) + 1     #这句话和下面三句效果相同
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+'''按照某个特征的某个取值划分数据集'''
+def splitDataSet(dataSet,axis,value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+'''遍历数据集，选择最好的特征划分方式'''
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set([featList])
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestFeature = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+'''多数表决'''
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        classCount[vote] = classList.count(vote)
+    sortedclassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedclassCount[0][0]
+
+
+'''创建树'''
+def createTree(dataSet,labels):   # 注意这里labels存储的为特征的标签，例如：身高，体重。
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        sublables = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), sublables)
+    return myTree
+
+'''获取叶结点数目和树的深度'''
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = firstStr
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ =='dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = firstStr
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisdepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisdepth = 1
+        if thisdepth > maxDepth:
+            maxDepth = thisdepth
+    return maxDepth
+
+'''构建分类器'''
+def classify(inputTree, featLables, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLables.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLables, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
